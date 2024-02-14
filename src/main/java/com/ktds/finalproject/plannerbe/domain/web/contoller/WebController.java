fdc7c16bc547aae09a98426b13e243f2b0673b94
Jueun Yoon <00-10-21@naver.com>
2024-02-14 19:08:09 +0900

    feat: 대시보드 구현

--- src/main/java/com/ktds/finalproject/plannerbe/domain/web/contoller/WebController.java
+++ src/main/java/com/ktds/finalproject/plannerbe/domain/web/contoller/WebController.java
@@ -1,18 +1,40 @@
 package com.ktds.finalproject.plannerbe.domain.web.contoller;
 
+import com.ktds.finalproject.plannerbe.domain.cleaning.checklist.service.CleaningTodoItemService;
+import com.ktds.finalproject.plannerbe.domain.cooking.ingredient.service.IngredientService;
+import com.ktds.finalproject.plannerbe.domain.cooking.shoppinglist.service.ShoppingListService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class WebController {
 
+    private final CleaningTodoItemService cleaningTodoItemService;
+    private final ShoppingListService shoppingListService;
+    private final IngredientService ingredientService;
+
     @GetMapping("/")
-    public String showIndex() {
+    public String showIndex(Model model) {
+
+        model.addAttribute("todolist", cleaningTodoItemService.findAllByOrderByCreatedTimeDesc());
+        model.addAttribute("shoppinglist", shoppingListService.findAllByOrderByItemNameDesc());
+        model.addAttribute("refrigeratorIngredients", ingredientService.findByStorageLocationOrderByCreatedTimeDesc("냉장고"));
+        model.addAttribute("freezerIngredients", ingredientService.findByStorageLocationOrderByCreatedTimeDesc("냉동고"));
+        model.addAttribute("roomIngredients", ingredientService.findByStorageLocationOrderByCreatedTimeDesc("실온"));
+
         return "index";
     }
 
     @GetMapping("/index")
-    public String showIndex2() {
+    public String showIndex2(Model model) {
+        model.addAttribute("todolist", cleaningTodoItemService.findAllByOrderByCreatedTimeDesc());
+        model.addAttribute("shoppinglist", shoppingListService.findAllByOrderByItemNameDesc());
+        model.addAttribute("refrigeratorIngredients", ingredientService.findByStorageLocationOrderByCreatedTimeDesc("냉장고"));
+        model.addAttribute("freezerIngredients", ingredientService.findByStorageLocationOrderByCreatedTimeDesc("냉동고"));
+        model.addAttribute("roomIngredients", ingredientService.findByStorageLocationOrderByCreatedTimeDesc("실온"));
         return "index";
     }
 
